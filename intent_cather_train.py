13e0426a1aa0ca1618832df04e6b5a90605484a1
Sergey Tihomirov <tikhomirov2000@inbox.ru>
2021-12-08 20:55:47 +0300

    upgrade data

--- intent_cather_train.py
+++ intent_cather_train.py
@@ -1,2 +1,24 @@
 import json
+from deeppavlov import configs, train_model
 
+if __name__ == '__main__':
+    train_data = json.load(open("intent_catcher_data/train.json"))
+    test_data = json.load(open("intent_catcher_data/test.json"))
+    valid_data = json.load(open("intent_catcher_data/valid.json"))
+
+    del train_data['opinion_request']
+    del test_data['opinion_request']
+    del valid_data['opinion_request']
+
+    # train_data['exit']
+
+    train_data['complement'] = [
+        "((you're)|(you are)|(you)) (such ){,1}((a wonderful)|(an amazing)|(a marvellous)|"
+        "(a magnificent)|(a great)|(a glorious)|(a lovely)|(a terrific)|(a delightful)"
+        "|(a fantastic)|(an incredible)|(a tremendous)|(a gorgeous)|(an awesome))( person){,1}\!{,1}"
+    ]
+    json.dump(train_data, open("intent_catcher_data/train.json", 'w'))
+    json.dump(test_data, open("intent_catcher_data/test.json", 'w'))
+    json.dump(valid_data, open("intent_catcher_data/valid.json", "w"))
+
+config = json.load(open(configs['intent_catcher']['intent_catcher']))
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import telebot
 from deeppavlov import build_model
 from deeppavlov.core.common.file import read_json
 
-bot = telebot.TeleBot('1968298410:AAG2yrIZy2C3CxdX5_PhF9BfBoyuf5ARjhg')
+bot = telebot.TeleBot('token')
 model_config = read_json('squad_ru_bert_infer.json')
-model = build_model(model_config,download=True)
+model = build_model(model_config, download=True)
+
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
@@ -15,12 +16,13 @@ def start_command(message):
 # ToDo: Перевести сообщения в нижний регистр
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    bot.send_message(message.from_user.id, model_training(message.text) )
+    bot.send_message(message.from_user.id, model_training(message.text))
+
 
 def model_training(request):
     model_answers = model(
         ['DandyGames это студия празработки игр основанная в 2021 году, идея основания появилась в 2019 году.'
-         'Студия аннонсировала 2 проекта LabsOfFate и GameOfGods, '
+         'Студия аннонсировала 2 проекта LabOfFate и GameOfGods, '
          'ведущий геймдизайнер студии какое-то время работал над проектом Virsen.'
          'Студия находится по адресу ул. Пушкина дом Колотушкина. '
          'Телефон для связи 8-800-352-11-12 '],
@@ -29,7 +31,9 @@ def model_training(request):
     accuracy = model_answers[2][0]
     answer = model_answers[0][0]
 
-    if(int(accuracy) < 1500):
+    if (int(accuracy) < 1500):
         answer = "Я не знаю"
     return (answer)
-bot.polling();
+
+
+bot.polling()
