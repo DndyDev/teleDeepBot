7eb20a0cecb2552e1c4fb84438b57ba890b789b2
SITihomirov <sirtikhomirov@gmail.com>
2021-12-08 16:15:38 +0300

    begin intent catcher

---
+++ consoleSQuAD.py
@@ -0,0 +1,20 @@
+from deeppavlov import build_model, configs
+from deeppavlov.core.common.file import read_json
+
+model_config = read_json('squad_ru_bert_infer.json')
+
+model = build_model(model_config,download=True)
+model_answers =  model(['DandyGames это студия празработки игр основанная в 2021 году, идея основания появилась в 2019 году.'
+                        'Студия аннонсировала 2 проекта LabsOfFate и GameOfGods, '
+                        'ведущий геймдизайнер студии какое-то время работал над проектом VirsenСтудия находится по адресу ул. Пушкина дом Колотушкина. '
+                        'Телефон для связи 8-800-352-11-12 '],
+                       ['кто такой денди'])
+
+accuracy = model_answers[2][0]
+
+# if (accuracy > 1500):
+#     print(model_answers)
+# else:
+#     print("I dont now")
+
+print(model_answers)
---
+++ main.py
@@ -0,0 +1,35 @@
+import telebot
+from deeppavlov import build_model
+from deeppavlov.core.common.file import read_json
+
+bot = telebot.TeleBot('1968298410:AAG2yrIZy2C3CxdX5_PhF9BfBoyuf5ARjhg')
+model_config = read_json('squad_ru_bert_infer.json')
+model = build_model(model_config,download=True)
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Привет, я бот команды игровой студии DandyGames !"
+                                      "Ты можешь задать любой вопрос о компании ")
+
+
+# ToDo: Перевести сообщения в нижний регистр
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    bot.send_message(message.from_user.id, model_training(message.text) )
+
+def model_training(request):
+    model_answers = model(
+        ['DandyGames это студия празработки игр основанная в 2021 году, идея основания появилась в 2019 году.'
+         'Студия аннонсировала 2 проекта LabsOfFate и GameOfGods, '
+         'ведущий геймдизайнер студии какое-то время работал над проектом Virsen.'
+         'Студия находится по адресу ул. Пушкина дом Колотушкина. '
+         'Телефон для связи 8-800-352-11-12 '],
+        [request])
+
+    accuracy = model_answers[2][0]
+    answer = model_answers[0][0]
+
+    if(int(accuracy) < 1500):
+        answer = "Я не знаю"
+    return (answer)
+bot.polling();
